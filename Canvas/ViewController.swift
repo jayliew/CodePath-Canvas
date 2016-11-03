db271b845e6b83d4f8b89d0b63fd98139cde763d
Jay Liew <jaysern@gmail.com>
2016-11-02 23:01:12 -0700

    milestone 3 with bonuses

--- Canvas/ViewController.swift
+++ Canvas/ViewController.swift
@@ -13,12 +13,41 @@ class ViewController: UIViewController {
     @IBOutlet weak var trayView: UIView!
     
     var trayOriginalCenter: CGPoint!
+    var trayCenterWhenOpen: CGPoint!
+    var trayCenterWhenClosed: CGPoint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        trayCenterWhenOpen = trayView.center
+        trayCenterWhenClosed = CGPoint(x: trayView.center.x,
+                                       y: trayView.center.y + (
+                                        self.trayView.frame.size.height - 40))
+        
     }
 
+    @IBAction func onTrayTap(_ sender: AnyObject) {
+        if (trayCenterWhenOpen == trayView.center){
+            UIView.animate(withDuration: 0.5, // going down
+                delay: 0,
+                usingSpringWithDamping: 0.5,
+                initialSpringVelocity: 0,
+                options: .curveEaseIn,
+                animations: {
+                    self.trayView.center = self.trayCenterWhenClosed
+                }, completion: nil)
+
+        }else{
+            UIView.animate(withDuration: 0.5, // going up
+                delay: 0,
+                usingSpringWithDamping: 0.5,
+                initialSpringVelocity: 0,
+                options: .curveEaseIn,
+                animations: {
+                    self.trayView.center = self.trayCenterWhenOpen
+                }, completion: nil)
+        }
+    }
+    
     @IBAction func onTrayPanGesture(_ sender: UIPanGestureRecognizer) {
         
         let point = sender.location(in: self.view)
@@ -27,7 +56,28 @@ class ViewController: UIViewController {
             trayOriginalCenter = trayView.center
             print("Gesture began at: \(point)")
         }else if sender.state == .changed {
-            trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + sender.translation(in: self.view).y)
+            if(sender.velocity(in: trayView).y > 0){
+                
+                UIView.animate(withDuration: 0.5, // going down
+                               delay: 0,
+                               usingSpringWithDamping: 0.5,
+                               initialSpringVelocity: 0,
+                               options: .curveEaseIn,
+                               animations: {
+                                self.trayView.center = self.trayCenterWhenClosed
+                    }, completion: nil)
+                
+            }else{
+                UIView.animate(withDuration: 0.5, // going up
+                    delay: 0,
+                    usingSpringWithDamping: 0.5,
+                    initialSpringVelocity: 0,
+                    options: .curveEaseIn,
+                    animations: {
+                        self.trayView.center = self.trayCenterWhenOpen
+                    }, completion: nil)
+            }
+            
             print("Gesture changed at: \(point)")
         }else if sender.state == .ended {
             print("Gesture ended at: \(point)")
